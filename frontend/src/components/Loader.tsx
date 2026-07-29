ab4599ba04b2e1173c5f72c4c9480c0c44a87758
kuldeep456789 <kuldeep456789>
2026-07-29 10:26:16 +0530

    new features added

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -7,39 +7,18 @@ interface LoaderProps {
 export default function Loader({ fullScreen = true }: LoaderProps) {
   return (
     <div className={fullScreen ? "autotrade-loader-overlay" : "autotrade-loader-inline"}>
-      <svg
-        width="90"
-        height="90"
-        viewBox="0 0 100 100"
-        className="autotrade-loader-svg"
-      >
-        <defs>
-          <clipPath id="autotrade-vclip">
-            <polygon points="10,15 50,85 90,15 78,15 50,63 22,15" />
-          </clipPath>
-          <linearGradient id="autotrade-sweepgrad" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="0%" stopColor="#800000" />
-            <stop offset="45%" stopColor="#ee1010" />
-            <stop offset="55%" stopColor="#ff6060" />
-            <stop offset="65%" stopColor="#ee1010" />
-            <stop offset="100%" stopColor="#800000" />
-          </linearGradient>
-        </defs>
-        <polygon
-          points="10,15 50,85 90,15 78,15 50,63 22,15"
-          fill="#1C274C"
+      <div className="autotrade-loader-container">
+        {/* Animated outer ring spinner */}
+        <div className="autotrade-loader-ring" />
+
+        {/* Brand logo image */}
+        <img
+          src="/img/logo.png"
+          alt="AutoTrade Loading..."
+          className="autotrade-loader-logo"
         />
-        <g clipPath="url(#autotrade-vclip)">
-          <rect
-            x="0"
-            y="0"
-            width="100"
-            height="60"
-            fill="url(#autotrade-sweepgrad)"
-            className="autotrade-sweep-rect"
-          />
-        </g>
-      </svg>
+      </div>
+      <p className="autotrade-loader-text">Loading AutoTrade...</p>
     </div>
   );
 }
